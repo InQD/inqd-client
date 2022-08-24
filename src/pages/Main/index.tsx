f90c805474edd48d8565a529711b944861192a65
kyhyun <k29176@gmail.com>
2022-08-24 19:12:07 +0900

    #3 feat : Add fade-in and fade-out animation and fill-color effect

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,22 +1,42 @@
+import { useState } from 'react'
 import styles from './main.module.scss'
+import cx from 'classnames'
+
 import { SettingIcon, UpArrowIcon } from 'assets/svgs'
 import logoImg from '../../assets/images/logo.png'
 
+/**
+ * @desc 인터뷰를 매개변수로 받아 스타일을 지정하는 유틸 함수
+ *  */
+const getAnimation = (interview: boolean | null) => {
+  switch (interview) {
+    case null:
+      return styles.fadein
+    case true:
+      return styles.fadeout
+    default:
+      throw new Error(`unknown info : ${interview}`)
+  }
+}
+
 const Main = () => {
+  const [interviewQuestions, setInterviewQuestions] = useState(null)
+  const haveQuestion = interviewQuestions ?? null
   return (
     <section className={styles.main}>
       <header className={styles.header}>
-        <div className={styles.floatingMessage}>
+        <div className={cx(styles.floatingMessage, getAnimation(haveQuestion))}>
           <p>
             면접 질문이 없어요! <br />
             설정에서 질문을 등록해주세요.
           </p>
           <UpArrowIcon className={styles.upArrowIcon} />
         </div>
+
         <SettingIcon className={styles.settingIcon} />
       </header>
 
-      <main className={styles.logoInfo}>
+      <main className={cx(styles.logoInfo, { [styles.fillColor]: haveQuestion })}>
         <div className={styles.logoTitle}>
           <p className={styles.topText}>InQD 인큐디</p>
           <h1>인터뷰 헬퍼</h1>
@@ -25,7 +45,7 @@ const Main = () => {
         <img className={styles.logo} src={logoImg} alt='로고' />
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={cx(styles.footer, { [styles.fillColor]: haveQuestion })}>
         <button type='button'>면접 시작!</button>
       </footer>
     </section>
