d6830dcae1e80ed3543f4be4b811922667ffa82f
kyhyun <k29176@gmail.com>
2022-08-29 01:44:01 +0900

    chore : Add new svg icon

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as DeleteAllIcon } from './delete-all.svg'
 export { ReactComponent as SettingIcon } from './setting.svg'
 export { ReactComponent as UpArrowIcon } from './up-arrow.svg'
+export { ReactComponent as RightArrowIcon } from './right-arrow.svg'
