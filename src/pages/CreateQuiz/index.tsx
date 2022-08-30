55c09e250435305c5eb002672eaeb0a6fddf99b4
kyhyun <k29176@gmail.com>
2022-08-30 14:20:56 +0900

    #16 refactor : Apply mandatory question button following category color
    해당 페이지에 관련된 컴포넌트들의 구조적인 부분을 다수 변경

--- src/pages/CreateQuiz/index.tsx
+++ src/pages/CreateQuiz/index.tsx
@@ -1,20 +1,46 @@
 import styles from './createQuiz.module.scss'
 import QuizTextarea from './QuizTextarea'
 import QuizCategory from './QuizCategory'
+import { cx } from 'styles'
+import { useState } from 'react'
 
 const CreateQuiz = () => {
+  const [category, setCategory] = useState('personality')
+  const getCategory = (value: string) => {
+    switch (value) {
+      case '인성':
+        return setCategory('personality')
+      case '기술':
+        return setCategory('technical')
+      default:
+        throw new Error(`unknown category : ${value}`)
+    }
+  }
+
+  const getStyles = () => {
+    switch (category) {
+      case 'personality':
+        return styles.personality
+      case 'technical':
+        return styles.technical
+      default:
+        throw new Error(`unknown category : ${category}`)
+    }
+  }
+
   return (
     <div className={styles.createQuiz}>
       <header className={styles.header}>Header Area</header>
       <main className={styles.main}>
         <div className={styles.display}>Display Area</div>
-        <QuizCategory />
+        <QuizCategory getCategory={getCategory} />
+
         {/* 필수질문 지정 */}
         <section className={styles.essential}>
           <label className={styles.essentialLabel}>
             필수 질문으로 지정
             <input type='checkbox' name='essential-question' />
-            <div className={styles.checkmark} />
+            <div className={cx(styles.checkmark, getStyles())} />
           </label>
         </section>
 
